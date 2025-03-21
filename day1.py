880bab118f735456700ff911a7b22e3ac9db3d1d
Hydra <scilentknight512@gmail.com>
2025-03-21 10:36:50 +0545

    first commit

---
+++ day1.py
@@ -0,0 +1,12 @@
+# import numpy as np
+
+# my_array = np.array([[1,2,3],[6,8,7]])  #2d array
+# print(my_array)
+
+# # input sattement
+# name = input("Enter your name: ")
+# print()
+# print("my name is", name)
+
+
+
---
+++ day2.py
@@ -0,0 +1,91 @@
+# print(10**2)
+# a = pow(10, 2)  # 10^2
+# print(a)
+
+
+# # logical operator
+# a = 10
+# b = a>5 and a<100
+# print(b)
+
+# c = a>5 or a<5
+# print(c)
+# d = not(c)
+# print(d)
+
+
+
+# # Identity operators
+# is
+# not in 
+
+# # Members operator
+# in
+# not in
+
+# a = 10
+# b = 10
+# print(a is b)  # True (same memory location for small integers)
+# print(a is not b)
+
+
+# x = ["apple", "banana"]
+# y = ["apple", "banana"]
+# z = x
+# print(x is z)   # x memory location and z memory location is same
+
+# print(x is y)   # x and y memory location is different
+
+# print(x == y)   # x values and y values are same or equal
+
+
+# a = [[10, 5, 6, 7, 14],10, 5, 6, 7, 14]
+# b = [10, 5, 6, 7, 14]
+
+# print(b in a) 
+
+
+# # Assignment operator
+# x = 5
+# print(x)
+# x+= 5
+# print(x)
+# x-=5
+# print(x)
+# x*=5
+# print(x)
+# x/=5
+# print(x)
+# x%=5
+# print(x)
+# x**=5
+# print(x)
+
+# print("We have Python's class today")
+# print('We have class for "variables"')
+# print("We have Python's class \"today\"")
+
+# Multiline strings
+# message = '''my name \
+# is 
+# prakash pathak\
+#     '''
+# print(message)
+
+x ="Hello World"
+print(x[0])
+x[0] = 'M' # string is immuitable(can not be change)
+
+
+x = "Hello World"
+b = x.replace('W','w')
+print(b)
+print(x)
+c = x.split(' ')
+print(c)
+
+
+x = "Hello World!"
+print(len(x))
+
+
